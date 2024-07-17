4101b73f198f5623ab9a0a024abc525c8106fc57
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-07-17 20:33:47 +1000

    useDragging hook/ draggable block

--- src/whiteboard/elements/BasicTypeBlock.tsx
+++ src/whiteboard/elements/BasicTypeBlock.tsx
@@ -1,13 +1,21 @@
-import { FC } from 'react';
+import { FC, useRef } from 'react';
 import { BasicType } from './types';
+import { useDragging } from './hooks';
 
 type Props = {
     value: BasicType;
 };
 
 const BasicTypeBlock: FC<Props> = ({ value }: { value: BasicType }) => {
+    const blockRef = useRef<HTMLSpanElement>(null);
+
+    useDragging(blockRef);
+
     return (
-        <span className="cursor-pointer absolute rounded-lg bg-violet-300 h-12 w-24 flex items-center justify-center">
+        <span
+            ref={blockRef}
+            className="cursor-pointer absolute rounded-lg bg-violet-300 h-12 w-24 flex items-center justify-center"
+        >
             <p className="font-bold">{value}</p>
         </span>
     );
---
+++ src/whiteboard/elements/hooks.ts
@@ -0,0 +1,82 @@
+import { RefObject, useCallback, useEffect } from 'react';
+
+export const useDragging = (ref: RefObject<HTMLElement>) => {
+    const startDragging = useCallback(
+        (oe: MouseEvent) => {
+            let prevX = oe.clientX;
+            let prevY = oe.clientY;
+
+            const drag = (e: MouseEvent) => {
+                e.preventDefault();
+                if (ref.current !== null) {
+                    const deltaX = e.clientX - prevX;
+                    const deltaY = e.clientY - prevY;
+                    ref.current.style.left =
+                        clamp(
+                            parseInt(ref.current.style.left.split('px')[0]) +
+                                deltaX,
+                            0,
+                            window.innerWidth - ref.current.clientWidth
+                        ) + 'px';
+                    ref.current.style.top =
+                        clamp(
+                            parseInt(ref.current.style.top.split('px')[0]) +
+                                deltaY,
+                            0,
+                            window.innerHeight - ref.current.clientHeight
+                        ) + 'px';
+                }
+                prevX = e.clientX;
+                prevY = e.clientY;
+            };
+
+            const dragFinish = () => {
+                window.removeEventListener('mousemove', drag);
+                window.removeEventListener('mouseup', dragFinish);
+            };
+
+            window.addEventListener('mousemove', drag);
+            window.addEventListener('mouseup', dragFinish);
+        },
+        [ref]
+    );
+
+    useEffect(() => {
+        const elem = ref.current;
+        if (elem) {
+            if (elem.style.top === '') {
+                elem.style.top = elem.getBoundingClientRect().top + 'px';
+            }
+            if (elem.style.left === '') {
+                elem.style.left = elem.getBoundingClientRect().left + 'px';
+            }
+            elem.addEventListener('mousedown', startDragging);
+            const observer = new ResizeObserver(() => {
+                if (
+                    parseInt(elem.style.left) + elem.clientWidth >
+                    window.innerWidth
+                ) {
+                    console.log('left');
+                    elem.style.left =
+                        window.innerWidth - elem.clientWidth + 'px';
+                }
+                if (
+                    parseInt(elem.style.top) + elem.clientHeight >
+                    window.innerHeight
+                ) {
+                    elem.style.top =
+                        window.innerHeight - elem.clientHeight + 'px';
+                }
+            });
+            observer.observe(document.body);
+            return () => {
+                elem.removeEventListener('mousedown', startDragging);
+                observer.disconnect();
+            };
+        }
+    }, [ref, startDragging]);
+};
+
+export const clamp = (value: number, min: number, max: number): number => {
+    return Math.max(min, Math.min(value, max));
+};
